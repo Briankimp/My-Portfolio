ee554ecefa09bb8263022768f22042a6baff7649
Briankimp <bkimathi973@gmail.com>
2024-09-05 17:43:43 +0300

    analytics corrected

--- NextJSPortfolio/thePortfolio/app/layout.tsx
+++ NextJSPortfolio/thePortfolio/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
-import {Analytics} from '@vercel/analytics';
+import Analytics from '@vercel/analytics';
 const jetbrains_Mono = JetBrains_Mono({ 
   subsets: ["latin"],
   weight: ["100","200", "300", "400", "500", "600", "700", "800"],
